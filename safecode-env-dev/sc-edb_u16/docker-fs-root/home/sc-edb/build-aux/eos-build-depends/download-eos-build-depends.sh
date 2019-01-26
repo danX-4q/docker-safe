a6fef1d320b82718755142f723666a8e76a59c8c
danX-4q <chenxd@bankledger.com>
2019-01-26 19:44:17 +0800

    add: remark eos-build-depends url

---
+++ safecode-env-dev/sc-edb_u16/docker-fs-root/home/sc-edb/build-aux/eos-build-depends/download-eos-build-depends.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#get url from eosio_build_ubuntu.sh
+
+wget https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.bz2
+wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-3.6.3.tgz
+wget https://github.com/mongodb/mongo-c-driver/releases/download/1.10.2/mongo-c-driver-1.10.2.tar.gz

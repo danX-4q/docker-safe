6814e7e4b5977b3a4f4a07907d4dd30e86dcae25
danX-4q <chenxd@bankledger.com>
2019-06-18 11:17:30 +0800

    run start-keosd.sh ok.

--- safecode-env-test/sc-et_u16/docker-fs-root/home/sc-test/script/service/start-keosd.sh
+++ safecode-env-test/sc-et_u16/docker-fs-root/home/sc-test/script/service/start-keosd.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+zpath=$(cd `dirname $0`; pwd)
+cd $zpath > /dev/null
+
+##############################
+
+#usage:
+#    $1: node name, which will be used to get node config 
+#        at dir ../node/$1/eosio.conf(or eosio.env)
+
 set -x
 
 NM=${1-"a"}
-CUR_PWD="$PWD"
+N_C_DIR="../node/${NM}/"
 
-cd $NM
+cd $N_C_DIR
 . eosio.conf
 cd -
 
-keosd -c ${KEOSD_DIR}config/config.ini -d ${KEOSD_DIR}data/ &
-
+keosd -c ${KEOSD_C_DIR}config.ini -d ${KEOSD_D_DIR} &

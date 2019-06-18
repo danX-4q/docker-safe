3b6c583164c82e0672253090019ddc5c071e9930
danX-4q <chenxd@bankledger.com>
2019-06-18 11:25:21 +0800

    run stop-xxx.sh ok.

--- safecode-env-test/sc-et_u16/docker-fs-root/home/sc-test/script/service/stop-keosd.sh
+++ safecode-env-test/sc-et_u16/docker-fs-root/home/sc-test/script/service/stop-keosd.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+zpath=$(cd `dirname $0`; pwd)
+cd $zpath > /dev/null
+
+##############################
 PP=$(pidof keosd)
 [[ $PP != "" ]] && kill $PP
 
--- safecode-env-test/sc-et_u16/docker-fs-root/home/sc-test/script/service/stop-nodeos.sh
+++ safecode-env-test/sc-et_u16/docker-fs-root/home/sc-test/script/service/stop-nodeos.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+zpath=$(cd `dirname $0`; pwd)
+cd $zpath > /dev/null
+
+##############################
+
 PP=$(pidof nodeos)
 [[ $PP != "" ]] && kill $PP
 

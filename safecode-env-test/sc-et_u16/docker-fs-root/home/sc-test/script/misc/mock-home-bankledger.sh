7859df14ad1d84bb2a26db4425bf560c3e52e10a
danX-4q <chenxd@bankledger.com>
2019-06-18 10:52:20 +0800

    add: mock-home-bankledger.sh

---
+++ safecode-env-test/sc-et_u16/docker-fs-root/home/sc-test/script/misc/mock-home-bankledger.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+zpath=$(cd `dirname $0`; pwd)
+cd $zpath > /dev/null
+
+cd ../../../bankledger
+#now change dir to /home/bankledger or .../docker-fs-root/home/bankledger
+
+function link_dir()
+{
+    tdir=$1
+    [ -e "$tdir" ] && { echo "error: ${PWD}/${tdir} has exists, please check it."; exit 1; }
+    ln -s "/home/bankledger/${tdir}"
+}
+
+link_dir eos/
+link_dir eosio.cdt/
+link_dir eosio.contracts/

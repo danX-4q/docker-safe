1aa8d689497e6d20486ede730ae599de1c3842bb
danX-4q <chenxd@bankledger.com>
2019-06-18 11:28:05 +0800

    run clean-nodeos-data.sh ok.

--- safecode-env-test/sc-et_u16/docker-fs-root/home/sc-test/script/service/clean-nodeos-data.sh
+++ safecode-env-test/sc-et_u16/docker-fs-root/home/sc-test/script/service/clean-nodeos-data.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+zpath=$(cd `dirname $0`; pwd)
+cd $zpath > /dev/null
+
+##############################
+
+#usage:
+#    $1: node name, which will be used to get node config 
+#        at dir ../node/$1/eosio.conf(or eosio.env)
+
 set -x
 
 NM=${1-"a"}
-CUR_PWD="$PWD"
+N_C_DIR="../node/${NM}/"
 
-cd $NM
+cd $N_C_DIR
 . eosio.conf
 cd -
 
+##############################
+
 ./stop-nodeos.sh
-rm -rf ${NODEOS_DIR}data/*
+rm -rf ${NODEOS_D_DIR}/{blocks,snapshots,state}

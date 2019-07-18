8d866fd8609cd106a489d17f0b453ac5a4f83930
danX-4q <chenxd@bankledger.com>
2019-07-18 11:49:01 +0800

    dc-script; run ok.

---
+++ safecode-env-dev/sc-edb_u16/dc-script/dc-login-bash.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+zpath=$(cd `dirname $0`; pwd)
+cd $zpath > /dev/null
+
+##############################
+
+#usage:
+#    $1: dc-script global config file
+#        default: ./dc.conf
+
+set -x
+
+DC_CONF=${1-"./dc.conf"}
+. $DC_CONF || { echo "error when load $DC_CONF"; exit 1; }
+cd "$DC_PRJ_ROOT_DIR" > /dev/null
+
+##############################
+
+docker-compose exec ${DC_SERVICE} bash

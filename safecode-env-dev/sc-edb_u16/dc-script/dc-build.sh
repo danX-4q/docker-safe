b705421103fb28d56e0aae51a1625e81a9023b35
danX-4q <chenxd@bankledger.com>
2019-07-18 13:41:34 +0800

    1. build when up; 2. do not commit git finger

---
+++ safecode-env-dev/sc-edb_u16/dc-script/dc-build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+zpath=$(cd `dirname $0`; pwd)
+cd $zpath > /dev/null
+
+##############################
+
+#usage:
+#    $1: dc-script global config file
+#        default: ./dc.conf
+
+set -x
+
+DC_CONF=${1-"./dc.conf"}
+. $DC_CONF || { echo "error when load $DC_CONF"; exit 1; }
+cd "$DC_PRJ_ROOT_DIR" > /dev/null
+
+##############################
+
+VAR_PREFIX="SC_EDB__"
+EVN_FILE="./docker-env.git"
+
+GIT_BRANCH=$(git branch -va | grep '^*' | awk '{print $2}')
+GIT_HASH=$(git log ${GIT_BRANCH} -n1 | grep '^commit' | awk '{print $2}')
+
+echo "${VAR_PREFIX}GIT_BRANCH=${GIT_BRANCH}" > ${EVN_FILE}
+echo "${VAR_PREFIX}GIT_HASH=${GIT_HASH}" >> ${EVN_FILE}
+
+docker-compose build

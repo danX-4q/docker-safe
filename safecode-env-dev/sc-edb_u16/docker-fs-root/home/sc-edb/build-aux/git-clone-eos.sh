b085fec7cddec0e14aa276cd6c9a8b1e832810e3
danX-4q <chenxd@bankledger.com>
2019-07-19 17:47:52 +0800

    del: noused files

--- safecode-env-dev/sc-edb_u16/docker-fs-root/home/sc-edb/build-aux/git-clone-eos.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-EOS_VER="v1.6.0"
-rm -rf eos-$EOS_VER eos-$EOS_VER.zip
-git clone -b $EOS_VER --single-branch https://github.com/EOSIO/eos.git --recursive eos-$EOS_VER
-zip -r eos-$EOS_VER.zip eos-$EOS_VER
-md5sum eos-$EOS_VER.zip
